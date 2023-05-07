2e3fcd33b64f3055da06661d587a81605e6a4904
Dale Menciro <dale_menciro@protonmail.com>
2023-05-08 00:45:47 +0800

    Pseudocodes for the arithmetic operations

--- Simple-App-Calculator.py
+++ Simple-App-Calculator.py
@@ -2,3 +2,29 @@
 import tkinter as tk
 #messagebox for error message
 from tkinter import messagebox
+
+# define arithmetic functions
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    # raise an exception if y is 0 to avoid division by zero error
+
+    return x / y
+
+# function to perform arithmetic operation according to user input
+def calculate():
+    try:
+        # get user inputs from Entry fields and option selected in Radiobutton
+
+        # execute selected arithmeric operation
+    #Exception for value error
+    except ValueError:
+    #Exception for zero division error
+    except ZeroDivisionError as e:
